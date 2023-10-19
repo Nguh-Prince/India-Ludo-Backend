be09674349b6b86ccd5e10e511744816f3f93443
prince <nguhprince1@gmail.com>
2023-10-19 13:11:39 +0100

    Refactored socket endpoints

--- index.mjs
+++ index.mjs
@@ -12,6 +12,7 @@ import { jsonParser, urlencodedParser } from "./utils/parsers.mjs";
 import jsonwebtoken from "jsonwebtoken";
 import Blacklist from "./models/blacklist.mjs";
 import Role from "./models/role.mjs";
+import { socketConnection } from "./utils/socket-io.mjs";
 
 const connectionString = process.env.ATLAS_URI || "";
 
@@ -28,13 +29,13 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
+socketConnection(new Server(server, {
   maxHttpBufferSize: 1e8,
   cors: {
     origin: "*",
     methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
   },
-});
+}));
 
 app.use(cors("*"));
 
@@ -78,70 +79,8 @@ app.use("/auth", authentication);
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  // res.sendFile(join(__dirname, 'index.html'));
+  // res.sendFile(join(process.cwd(), 'index.html'));
   return res.json({ message: "The server is live!" });
-});
-
-var colorsTaken = [];
-const numberOfPlayers = 2;
-
-io.on("connection", (socket) => {
-  io.emit("userConnected", socket.id);
-
-  console.log("Client connected");
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-
-  socket.on("move", (msg) => {
-    console.log("Client sent move");
-    console.log(msg);
-    socket.broadcast.emit("move", msg);
-  });
-
-  socket.on("diceNumber", (msg) => {
-    console.log(`Client sent diceNumber: ${msg}`);
-    socket.broadcast.emit("diceNumber", msg);
-  });
-
-  socket.on("joinGame", (msg) => {
-    console.log(`In index.js Gotten a joinGame message. colorsTaken before: `);
-    console.log(colorsTaken);
-
-    if (colorsTaken.includes(msg)) {
-      console.log("The user has chosen a color that has already been taken");
-    } else {
-      colorsTaken.push(msg);
-
-      if (colorsTaken.length >= numberOfPlayers) {
-        let sequence = getSequenceFromListOfColors(colorsTaken, 0);
-        io.emit("startGame", { sequence: sequence, first: colorsTaken[0] });
-      }
-
-      io.emit("colorsTaken", colorsTaken);
-    }
-
-    console.log(`In index.js Gotten a joinGame message. colorsTaken after: `);
-    console.log(colorsTaken);
-  });
-
-  socket.on("restartGame", (msg) => {
-    console.log("Gotten restartGame message");
-    colorsTaken = [];
-
-    // io.emit("colorsTaken", colorsTaken);
-    socket.broadcast.emit("restartGame");
-  });
-
-  socket.on("colorsTaken", () => {
-    console.log("Received colorsTaken event. Forwarding array: ");
-    console.log(colorsTaken);
-    io.emit("colorsTaken", colorsTaken);
-  });
-
-  socket.on("disconnect", (reason) => {
-    console.log(`Client disconnected`);
-  });
 });
 
 const PORT = 4000;
